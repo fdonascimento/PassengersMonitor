03111058d41bc095e64db04093c8e9dd2013f3a7
Fagner Fonseca <fagneolli@gmail.com>
2018-01-28 18:19:01 -0300

    correct model mapping

--- MonitorServer/GpsReceiver/locations/models.py
+++ MonitorServer/GpsReceiver/locations/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
-    latitude = models.FloatField(null=False,)
+    latitude = models.FloatField(null=False)
     longitude = models.FloatField(null=False)
     device_identification = models.IntegerField(null=False)
     date_time = models.DateTimeField(null=False)
